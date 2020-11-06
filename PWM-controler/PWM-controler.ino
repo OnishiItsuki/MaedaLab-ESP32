f9974cf11898c3b1b4b25b6170bd8b091e9fa2ce
Onishi Itsuki <onishi.itsuki@ist.osaka-u.ac.jp>
2020-11-06 11:41:55 +0900

    change how to set output pin (intager to define) and library name (Ticker.h to ESP32_Tickre.h)

--- PWM-controler/PWM-controler.ino
+++ PWM-controler/PWM-controler.ino
@@ -2,8 +2,7 @@
 // Itsuki Onishi
 // October 2020
 
-#include<stdio.h>
-#include "Ticker.h"
+#include <ESP32_Ticker.h>
 
 #define START_BYTE 0x0f
 #define SAMPLING_PERIOD 5.0f  // scale is ms
@@ -12,32 +11,32 @@
 
 #define PATHNAME_SIZE 512
 
+// set output pins
+// right side
+#define boomOutA 15
+#define boomOutB 2
+#define armOutA 0
+#define armOutB 4
+#define bucketOutA 16
+#define bucketOutB 17
+// left side
+#define slewingOutA 12
+#define slewingOutB 14
+#define rightWheelOutA 27
+#define rightWheelOutB 26
+#define leftWheelOutA 25
+#define leftWheelOutB 33
+
 uint8_t buffer[8];
 
 int outputBuffer[12];  // for setting output 0 or 1 to each Digital
 int bufferIndex = 0;
 int outProcCounter = 0;
 int outProcFlag = 0;
+int test_count = 0;
 
-//Ticker ticker;
-//Ticker input_test_data;
-
-// set output pins
-// right side
-int boomOutA = 15;
-int boomOutB = 2;
-int armOutA = 0;
-int armOutB = 4;
-int bucketOutA = 16;
-int bucketOutB = 17;
-// left side
-int slewingOutA = 12;
-int slewingOutB = 14;
-int rightWheelOutA = 27;
-int rightWheelOutB = 26;
-int leftWheelOutA = 25;
-int leftWheelOutB = 33;
-
+Ticker ticker(0);
+Ticker input_test_data(1);
 
 void set_test_buffer(void)
 {
@@ -96,12 +95,11 @@ void setup()
 
   // initalize serial port
   Serial.begin(9600);
-  int test_count = 0;
 
   // initial state is free run
   Serial.println("+");
-//  ticker.attach_us(SAMPLING_PERIOD/128.0f, PWM_processor);
-//  input_test_data.attach_us(SAMPLING_PERIOD, set_test_buffer);
+  ticker.attach_us(SAMPLING_PERIOD/128.0f, PWM_processor);
+  input_test_data.attach_us(SAMPLING_PERIOD, set_test_buffer);
 
 }
 
@@ -136,8 +134,10 @@ void event_func(void)
 
   // For test
   bufferIndex = 8;
-  for(int i=0; i<8; i++){
-    Serial.println("%d:%d ", i, buffer[i]);
+    Serial.println("check buffer");
+    for(int i=0; i<8; i++){
+    Serial.println(i);
+    Serial.println(buffer[i]);
   }
   uint8_t rx = buffer[0];
   // end of code for test
@@ -158,16 +158,19 @@ void event_func(void)
     bufferIndex = 0;
 
 //    check parity
-//    uint8_t serialXOR = buffer[1];
-//    for (int i=2; i<=6; i++) {
-//      serialXOR ^= buffer[i];
-//    }
-//    if (serialXOR != buffer[7]) {
-//      Serial.println("parity byte ERROR: XOR is %d, buffer byte is %d  ", serialXOR, buffer[7]);
-//      return;
-//    } else {
-//      outProcFlag = 1;
-//    }
+    uint8_t serialXOR = buffer[1];
+    for (int i=2; i<=6; i++) {
+      serialXOR ^= buffer[i];
+    }
+    if (serialXOR != buffer[7]) {
+      Serial.print("parity byte ERROR: XOR is ");
+      Serial.print(serialXOR);
+      Serial.print(" buffer byte is ");
+      Serial.println(buffer[7]);
+      return;
+    } else {
+      outProcFlag = 1;
+    }
   }
 }
 
@@ -180,10 +183,9 @@ void loop()
 
 //code for test
   if(test_count%1000 == 0) {
-    Serial.printf("+");
+    Serial.println("+");
     test_count = 0;
   }
 //end of code for test
 
-    }
 }
