b5aeda988c420dfc5666490131c6f2dc978e5789
OnishiItsuki <onishi.itsuki@ist.osaka-u.ac.jp>
2020-12-18 13:03:26 +0900

    finish making Tx with UDP part in Wifi_test_Tx.ino

--- WiFi_test_Tx/WiFi_test_Tx.ino
+++ WiFi_test_Tx/WiFi_test_Tx.ino
@@ -2,13 +2,23 @@
 #include <WiFiUdp.h>
 
 const char ssid[] = "ESP32_AP";
-const char password[] = "damedame";
+const char password[] = "esp32pass";
 
 static WiFiUDP udp;
 static const char *RxIP = "192.168.4.13";
 static const int RxPort = 10000;
-
 static const int localPort = 5000;
+
+uint8_t start_byte = 0x0f;
+uint8_t boom_byte = 0x01;
+uint8_t arm_byte = 0x02;
+uint8_t bucket_byte = 0x01;
+uint8_t slewing_byte = 0x04;
+uint8_t r_wheel_byte = 0x08;
+uint8_t l_wheel_byte = 0x10;
+uint8_t parity_byte = 0x00;
+
+uint8_t buffer[8];
 
 static void Wifi_setup()
 {
@@ -41,14 +51,52 @@ void setup() {
   Serial.println("");
 }
 
+static void AD_converter()
+{
+  // TODO: make AD converter
+  // Now put test value into buffer for test
+  buffer[0] = start_byte;
+  buffer[1] = boom_byte;
+  buffer[2] = arm_byte;
+  buffer[3] = bucket_byte;
+  buffer[4] = slewing_byte;
+  buffer[5] = r_wheel_byte;
+  buffer[6] = l_wheel_byte; 
+}
+
+static unsigned int comp_parity()
+{
+  // initialize parity buffer
+  buffer[7] = 0;
+
+//  compute parity buffer
+  uint8_t tmp = buffer[1];
+  for(int i=2; i<7; i++) {
+    tmp ^= buffer[i];
+  }
+  return tmp;
+}
+
 void loop()
 {
+  AD_converter();
   if (WiFi.status() != WL_CONNECTED){
     Serial.println("Disconnected");
+    Wifi_setup();
   } else {
-    udp.beginPacket(RxIP, RxPort);
-    udp.write('a');
-    udp.endPacket();  
+  buffer[7] = comp_parity();
+  
+  udp.beginPacket(RxIP, RxPort);
+  for(int i=0; i<8; i++) {
+    udp.write((unsigned char)(buffer[i]&0xff));    //packet sending
   }
-  delay(3000);
+  udp.endPacket();
+
+  Serial.println("print Buffer array");
+  for(int i=0; i<8; i++) {
+    Serial.println((unsigned char)(buffer[i]&0xff)); // to separate line
+  }
+
+  delay(1000);
+  }
 }
