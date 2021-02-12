9d41e76fdd071a428d29cad420fa721bb70bd8e4
Onishi Itsuki <onishi.itsuki@ist.osaka-u.ac.jp>
2021-02-12 10:56:59 +0900

    make AD_converter.ino in UDP_Tx

--- AD_converter/AD_converter.ino
+++ AD_converter/AD_converter.ino
@@ -1,5 +1,5 @@
-const int InputP = 33;
-const int InputN = 35;
+const int InputP = 35;
+const int InputN = 33;
 const int VOLT = 3.3;
 const int ANALOG_MAX = 4096;
 
@@ -11,8 +11,8 @@ void loop() {
   int read_P = analogRead(InputP);
   int read_N = analogRead(InputN);
 
-  float voltage_P = (long)read_P * VOLT * 1000 / ANALOG_MAX;
-  float voltage_N = (long)read_N * VOLT * 1000 / ANALOG_MAX;
+  float voltage_P = (float)read_P * VOLT * 1000 / ANALOG_MAX;
+  float voltage_N = (float)read_N * VOLT * 1000 / ANALOG_MAX;
   float voltage_sum = voltage_P + voltage_N;
 
   Serial.print("Positive:");
@@ -26,6 +26,11 @@ void loop() {
   Serial.print("Sum:");
   Serial.print(voltage_sum);
   Serial.println("mV");
+
+  Serial.print("P: ");
+  Serial.print(read_P);
+  Serial.print("  ,N: ");
+  Serial.println(read_N);
   
   delay(500);
 }
