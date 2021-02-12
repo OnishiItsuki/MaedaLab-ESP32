5f96e7e9fc12343f627b85d99747cd9776387b55
Onishi Itsuki <onishi.itsuki@ist.osaka-u.ac.jp>
2021-02-12 13:35:33 +0900

    make caribration code

--- AD_converter/AD_converter.ino
+++ AD_converter/AD_converter.ino
@@ -1,36 +1,144 @@
-const int InputP = 35;
-const int InputN = 33;
+#define InputP 35
+#define InputN 33
+
 const int VOLT = 3.3;
 const int ANALOG_MAX = 4096;
+const long CALIB_TIME = 1000;
 
-void setup() {
-  Serial.begin(115200);
+int AnaInputP = 0;
+int AnaInputN = 0;
+int AnaInputDiff = 0;
+
+int MaxAnaInput = 0;
+int MinAnaInput = 0;
+
+void Read_pin()
+{
+  AnaInputP = analogRead(InputP);
+  AnaInputN = analogRead(InputN);
+  AnaInputDiff = AnaInputP - AnaInputN;
 }
 
-void loop() {
-  int read_P = analogRead(InputP);
-  int read_N = analogRead(InputN);
+void Wait_key()
+{
+  while (true) {
+    if (Serial.available() > 0) {
+      while (Serial.available() > 0) {
+        Serial.read();
+      }
+      break;
+    }
+  }
+}
 
-  float voltage_P = (float)read_P * VOLT * 1000 / ANALOG_MAX;
-  float voltage_N = (float)read_N * VOLT * 1000 / ANALOG_MAX;
-  float voltage_sum = voltage_P + voltage_N;
+void Calibration()
+{
+  Serial.println("\nStart calibration.");
+  Serial.println("Keep tilting the lever in the positive direction and press a key\n");
+  //  Serial.println();
 
-  Serial.print("Positive:");
-  Serial.print(voltage_P);
-  Serial.print(" mV, ");
-  
-  Serial.print("Negative:");
-  Serial.print(voltage_N);
-  Serial.print(" mV, ");
+  //  Calibrate maximum value
+  while (true) {
+    Serial.println("-> Start calibration of positive direction");
+    Wait_key();
+    Read_pin();
+    if (AnaInputDiff < 0) {
+      Serial.println("This is the wrong direction. Tilt the lever in the opposit direction\n");
+      continue;
+    }
 
-  Serial.print("Sum:");
-  Serial.print(voltage_sum);
-  Serial.println("mV");
+    int TmpMax = 0;
+    long StartTime = millis();
+    Serial.println("Keep lever...");
+    while (true) {
+      Read_pin();
+      if (AnaInputDiff > TmpMax) {
+        TmpMax = AnaInputDiff;
+      }
+      long TmpTime = millis();
+      if (TmpTime - StartTime > CALIB_TIME) {
+        MaxAnaInput = TmpMax;
+        break;
+      }
+    }
+    Serial.println("Finish calibration of positive direction\n");
+    break;
+  }
 
-  Serial.print("P: ");
-  Serial.print(read_P);
-  Serial.print("  ,N: ");
-  Serial.println(read_N);
-  
+  //  Calibrate minimum value
+  while (true) {
+    Serial.println("-> Start calibration of negative direction");
+    Wait_key();
+    Read_pin();
+    if (AnaInputDiff > 0) {
+      Serial.println("This is the wrong direction. Tilt the lever in the opposit direction\n");
+      continue;
+    }
+    int TmpMin = 0;
+    long StartTime = millis();
+    Serial.println("Keep lever...");
+    while (true) {
+      Read_pin();
+      if (AnaInputDiff < TmpMin) {
+        TmpMin = AnaInputDiff;
+      }
+      long TmpTime = millis();
+      if (TmpTime - StartTime > CALIB_TIME) {
+        MinAnaInput = TmpMin;
+        break;
+      }
+    }
+    Serial.println("Finish calibration of negative direction\n");
+    break;
+  }
+
+  Serial.println("Finish calibration. Result is following");
+  Serial.print("Max value: ");
+  Serial.print(MaxAnaInput);
+  Serial.print("Min value: ");
+  Serial.print(MinAnaInput);
+  Serial.print("\n\n");
+}
+
+uint8_t AD_converter(float AnaPosSign, float AnaNegSign)
+{
+
+}
+
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Calibration();
+}
+
+void loop()
+{
+  Serial.println("a");
+  //
+  //  Read_pin();
+  //
+  //  float voltage_P = AnaInputP * VOLT * 1000 / ANALOG_MAX;
+  //  float voltage_N = AnaInputN * VOLT * 1000 / ANALOG_MAX;
+  //  float voltage_sum = voltage_P - voltage_N;
+  //
+  //  Serial.print("Positive:");
+  //  Serial.print(voltage_P);
+  //  Serial.print(" mV, ");
+  //
+  //  Serial.print("Negative:");
+  //  Serial.print(voltage_N);
+  //  Serial.print(" mV, ");
+  //
+  //  Serial.print("Sum:");
+  //  Serial.print(voltage_sum);
+  //  Serial.println("mV");
+  //
+  //  Serial.print("P: ");
+  //  Serial.print(AnaInputP);
+  //  Serial.print("  ,N: ");
+  //  Serial.println(AnaInputN);
+
   delay(500);
 }
--- UDP_Tx/AD_converter.ino
+++ UDP_Tx/AD_converter.ino
@@ -1,1 +1,36 @@
+const int InputP = 35;
+const int InputN = 33;
+const int VOLT = 3.3;
+const int ANALOG_MAX = 4096;
 
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int read_P = analogRead(InputP);
+  int read_N = analogRead(InputN);
+
+  float voltage_P = (float)read_P * VOLT * 1000 / ANALOG_MAX;
+  float voltage_N = (float)read_N * VOLT * 1000 / ANALOG_MAX;
+  float voltage_sum = voltage_P + voltage_N;
+
+  Serial.print("Positive:");
+  Serial.print(voltage_P);
+  Serial.print(" mV, ");
+  
+  Serial.print("Negative:");
+  Serial.print(voltage_N);
+  Serial.print(" mV, ");
+
+  Serial.print("Sum:");
+  Serial.print(voltage_sum);
+  Serial.println("mV");
+
+  Serial.print("P: ");
+  Serial.print(read_P);
+  Serial.print("  ,N: ");
+  Serial.println(read_N);
+  
+  delay(500);
+}
