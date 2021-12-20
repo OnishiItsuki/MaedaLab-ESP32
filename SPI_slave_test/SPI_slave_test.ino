81c7f77986d6ca6534fb39bfb3a8671d82c59d6a
Onishi Itsuki <onishi.itsuki@ist.osaka-u.ac.jp>
2021-12-20 11:41:44 +0900

    share

--- SPI_slave_test/SPI_slave_test.ino
+++ SPI_slave_test/SPI_slave_test.ino
@@ -2,6 +2,11 @@
 
 ESP32DMASPI::Slave slave;
 
+static const uint8_t SCK_slave=14;
+static const uint8_t MISO_slave=12;
+static const uint8_t MOSI_slave=13;
+static const uint8_t CS_slave=15;
+
 static const int MSG_SIZE = 20;
 uint8_t *s_message_buf;
 uint8_t *r_message_buf;
@@ -37,7 +42,7 @@ void setup()
     slave.setDMAChannel(2); // 専用メモリの割り当て（1か2のみ)
     slave.setQueueSize(1);  // キューサイズ　とりあえず1
     // HSPI(SPI2) のデフォルトピン番号は CS: 15, CLK: 14, MOSI: 13, MISO: 12
-    slave.begin(); // 引数を指定しなければデフォルトのSPI（SPI2,HSPIを利用）
+    slave.begin(SCK_slave, MISO_slave, MOSI_slave, CS_slave); // 引数を指定しなければデフォルトのSPI（SPI2,HSPIを利用）
     
     Serial.print("[Send] ");
     for (int i = 0; i < MSG_SIZE; i++)
