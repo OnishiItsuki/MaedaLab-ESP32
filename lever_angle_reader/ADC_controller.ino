15fda8e3fb1f8a7d30987567e703cfd33b95e2ca
Onishi Itsuki <onishi.itsuki@ist.osaka-u.ac.jp>
2022-02-21 18:17:05 +0900

    change the structure of functions

--- lever_angle_reader/ADC_controller.ino
+++ lever_angle_reader/ADC_controller.ino
@@ -9,21 +9,35 @@
 #define SCL_PIN 22
 #endif
 
+#define MAX_SLAVE_RESPONSE_LENGTH 3
+
 // ADC wait 7bit address but Wire.h send 16bit.
-// Following implementation cam have bugs.
+// Following implementation may have bugs.
 #define ADC_ADDR 0x08
 
 void ADC_init()
 {
     Serial.println("ADC_init is not implemented");
+    // Wire.begin();
 }
 
-void send_read_signal_to_ADC()
+void _send_read_signal() // step2
 {
     Serial.println("send_read_signal_to_ADC is not implemented");
 }
 
-void read_voltage_from_ADC(uint8_t buffer[])  // voltage_buffer[NUM_CH]
+void _receive_voltage_values(int buffer[]) // voltage_buffer[NUM_CH], step3
 {
-    Serial.println("read_voltage_from_ADC is not implemented");
+    Serial.println("receive_voltage_from_ADC is not implemented");
+    Wire.requestFrom(ADC_ADDR, MAX_SLAVE_RESPONSE_LENGTH);
+    for (int i = 0; i < NUM_CH; i++)
+    {
+        buffer[i] = Wire.read();
+    }
+}
+
+void read_voltage_from_ADC(int buffer[]) // voltage_buffer[NUM_CH]
+{
+    _send_read_signal();             // step2
+    _receive_voltage_values(buffer); // step3
 }
--- lever_angle_reader/UDP_Tx.ino
+++ lever_angle_reader/UDP_Tx.ino
@@ -14,8 +14,9 @@ static const char RxIP[] = "192.168.4.13";
 static const int RxPort = 10000;
 static const int localPort = 5000;
 
-static void Wifi_setup(Sting message)
+static void _Wifi_setup(Sting message)
 {
+  Serial.println(message);
   Serial.print("Connecting to ");
   Serial.print(ssid);
   Serial.print("  , ID is ");
@@ -36,10 +37,20 @@ static void Wifi_setup(Sting message)
   }
 }
 
+void _signal_buffer_init()
+{
+  for (int i = 0; i < buffer_size; i++)
+  {
+    signal_buffer[i] = 0;
+  }
+}
+
 void UDP_init()
 {
+  _signal_buffer_init();
+
   // WiFi setup
-  Wifi_setup("WiFi connecting.");
+  _Wifi_setup("WiFi connecting.");
   udp.begin(localPort);
 
   Serial.println("\n\n ********************* ");
@@ -54,44 +65,48 @@ void UDP_init()
 
   disp_clear();
   disp_add_string(10, 10, "WiFi connection successful");
-  String ip_port = (String)RxIP;
+  String ip_port = String(RxIP);
   ip_port.append(":");
-  ip_port.append((String)RXPort);
+  ip_port.append(String(RXPort));
   disp_add_string(10, 30, ip_port);
 }
 
-uint8_t compute_parity(uint8_t buffer[]) // signal_buffer[buffer_size]
+uint8_t _compute_signal_value_from_voltage(int voltage_value) // voltage[i], step4
+{
+  Serial.println("_compute_signal_value_from_voltage is not implemented");
+  return (uint8_t)voltage_value;
+}
+
+uint8_t _compute_parity(uint8_t buffer[]) // signal_buffer[buffer_size]
 {
   // initialize parity bit
   buffer[buffer_size - 1] = 0;
 
   //  compute parity bit
-  uint8_t parity_bit = buffer[1];
-  for (int i = 2; i < buffer_size - 1; i++)
+  uint8_t parity_bit = 0;
+  for (int i = 1; i < buffer_size - 1; i++)
   {
     parity_bit ^= buffer[i];
   }
   return parity_bit;
 }
 
-void generate_udp_signal_from_volrage(uint8_t voltage[]) // voltage_buffer[NUM_CH]
+void _generate_udp_signal_from_volrage(int voltage[]) // voltage_buffer[NUM_CH], step4
 {
   signal_buffer[0] = start_bit;
   for (int i = 1; i < buffer_size - 1; i++)
   {
-    signal_signal[i] = voltage[i];
+    signal_signal[i] = _compute_signal_value_from_voltage(voltage[i]);
   }
-  signal_buffer[buffer_size - 1] = compute_parity(voltage);
+  signal_buffer[buffer_size - 1] = _compute_parity(voltage);
 }
 
-void send_udp_signal(uint8_t voltage[]) // voltage_buffer[NUM_CH]
+void _send_signal() // step5
 {
-  generate_udp_signal_from_volrage(voltage);
-
   if (WiFi.status() != WL_CONNECTED)
   {
     Serial.println("WiFi disconnected. Reconnecting");
-    Wifi_setup("WiFi disconnected. Reconnecting");
+    _Wifi_setup("WiFi disconnected. Reconnecting");
   }
   else
   {
@@ -102,4 +117,11 @@ void send_udp_signal(uint8_t voltage[]) // voltage_buffer[NUM_CH]
     }
     udp.endPacket();
   }
+  _signal_buffer_init();
+}
+
+void send_udp_signal(int voltage[]) // voltage_buffer[NUM_CH]
+{
+  _generate_udp_signal_from_volrage(voltage); // step4
+  _send_signal();                             // step5
 }
