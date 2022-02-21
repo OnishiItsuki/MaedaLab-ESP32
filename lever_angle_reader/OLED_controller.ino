d8d186b759935a1fa07084fa5deee90def9efc05
Onishi Itsuki <onishi.itsuki@ist.osaka-u.ac.jp>
2022-02-21 18:13:54 +0900

    make usefull display function

--- lever_angle_reader/OLED_controller.ino
+++ lever_angle_reader/OLED_controller.ino
@@ -21,13 +21,21 @@
 #define DISP_HEIGHT 64
 #define FONT ArialMT_Plain_16
 
+#define FONT_SIZE_HEIGHT 20
+#define FONT_SIZE_WIDTH 10
+static const max_num_char_width = DISP_WIDTH / FONT_SIZE_WIDTH;
+
+static const indent_x = 5;
+static const indent_y = 5;
+
 SSD1306 display(OLED_ADDR, SDA_PIN, SCL_PIN);
 
 void OLED_init()
 {
+  disp_init();
   disp_all_pixel();
   delay(1000);
-  disp_init();
+  disp_clear();
 }
 
 void disp_init()
@@ -43,7 +51,6 @@ void disp_clear()
 
 void disp_all_pixel()
 {
-  display.init();
   display.fillRect(0, 0, DISP_WIDTH, DISP_HEIGHT);
   display.display();
 }
@@ -54,8 +61,34 @@ void disp_add_string(int16_t x, int16_t y, String message)
   display.display();
 }
 
-void disp_show_string(int16_t x, int16_t, y, String message)
+void disp_show_string(String message) // TODO: debug
 {
   disp_clear();
-  disp_add_string(x, y, message)
+  int line_index = 0;
+
+  int str_index = 0;
+  while (str_index < message.length())
+  {
+    String string_line = "";
+    for (int i = 0; i < max_num_char_width; i++)
+    {
+      static int pointer = str_index + i;
+      char char1 = message.charAt(pointer);
+      char char2 = message.charAt(pointer + 1);
+
+      if (char1 == '\\' and char2 == 'n')
+      {
+        break;
+      }
+      else
+      {
+        string_line += message[pointer];
+      }
+
+      int16_t y = indent_y + (FONT_SIZE_HEIGHT * line_index);
+      disp_add_string(indent_x, y, string_line);
+      str_index = pointer + 2;
+    }
+
+  }
 }
